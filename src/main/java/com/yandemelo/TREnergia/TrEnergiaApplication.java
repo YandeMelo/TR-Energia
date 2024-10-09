640d5fb43fcc82b9adea537a04243de9e3078345
Yan Melo <yandemelo@yahoo.com.br>
2024-10-09 20:12:37 -0300

    Dependência e documentação com Swagger

--- src/main/java/com/yandemelo/TREnergia/TrEnergiaApplication.java
+++ src/main/java/com/yandemelo/TREnergia/TrEnergiaApplication.java
@@ -3,7 +3,11 @@ package com.yandemelo.TREnergia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "TR Energia", version = "1.0", description = "API desenvolvida para o gerenciamento de Usuários."))
 public class TrEnergiaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/yandemelo/TREnergia/controllers/UsuarioController.java
+++ src/main/java/com/yandemelo/TREnergia/controllers/UsuarioController.java
@@ -17,6 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.yandemelo.TREnergia.dto.UsuarioDTO;
 import com.yandemelo.TREnergia.services.UsuarioService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 @RestController
@@ -26,36 +30,65 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Operation(summary = "Cadastrar Usuário")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Ok"),
+        @ApiResponse(responseCode = "422", description = "Unprocessable Entity", content = @Content()),
+        @ApiResponse(responseCode = "409", description = "Conflict", content = @Content())
+    })
     @PostMapping("/cadastrar")
     public ResponseEntity<UsuarioDTO> cadastrarUsuario(@RequestBody @Valid UsuarioDTO dto){
         dto = usuarioService.cadastrar(dto);
         return ResponseEntity.ok(dto);
     }
 
+    @Operation(summary = "Consultar Usuário por Id")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Ok"),
+        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content())
+    })
     @GetMapping("/consultarId/{id}")
     public ResponseEntity<UsuarioDTO> consultarPorId(@PathVariable UUID id){
         UsuarioDTO dto = usuarioService.consultarPorId(id);
         return ResponseEntity.ok(dto);
     }
 
+    @Operation(summary = "Consultar Usuário por Cpf")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Ok"),
+        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content())
+    })
     @GetMapping("/consultarCpf/{cpf}")
     public ResponseEntity<UsuarioDTO> consultarPorId(@PathVariable String cpf){
         UsuarioDTO dto = usuarioService.consultarPorCpf(cpf);
         return ResponseEntity.ok(dto);
     }
 
+    @Operation(summary = "Consultar Usuário por Id")
+    @ApiResponse(responseCode = "200", description = "Ok")
     @GetMapping("/listar")
     public ResponseEntity<List<UsuarioDTO>> listarUsuarios(){
         List<UsuarioDTO> usuarios = usuarioService.listarUsuarios();
         return ResponseEntity.ok(usuarios);
     }
 
+    @Operation(summary = "Atualizar Dados do Usuário")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Ok"),
+        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content()),
+        @ApiResponse(responseCode = "422", description = "Unprocessable Entity", content = @Content())
+    })
     @PutMapping("/atualizar/{id}")
     public ResponseEntity<UsuarioDTO> atualizarUsuario(@PathVariable UUID id, @RequestBody @Valid UsuarioDTO dto){
         dto = usuarioService.atualizarUsuario(id, dto);
         return ResponseEntity.ok(dto);
     }
 
+    @Operation(summary = "Excluir Usuário")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "No Content"),
+        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content())
+    })
     @DeleteMapping("/excluir/{id}")
     public ResponseEntity<Void> excluirUsuario(@PathVariable UUID id){
         usuarioService.excluirUsuario(id);

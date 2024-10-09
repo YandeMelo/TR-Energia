3e951c9408c938bb81f9c0fb911483c0238af206
Yan Melo <yandemelo@yahoo.com.br>
2024-10-09 16:46:32 -0300

    Adicionando dependências e conectando com o banco de dados

---
+++ src/main/java/com/yandemelo/TREnergia/TrEnergiaApplication.java
@@ -0,0 +1,13 @@
+package com.yandemelo.TREnergia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrEnergiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrEnergiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yandemelo/TREnergia/TrEnergiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yandemelo.TREnergia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrEnergiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
